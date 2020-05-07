af8146c12b063460fff38c68bc3c513d2aaba483
ShubhamRanaDev <shubhamrana.dev@gmail.com>
2020-05-07 16:11:23 -0400

    Shubham's commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There Snehal</Text>
+      <Text>Hello There Shubham</Text>
     </View>
   );
 }
