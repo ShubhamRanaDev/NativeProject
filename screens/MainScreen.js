5aefb57b132f2c672d98f3afd7c87b08999b84ac
sneh96 <snehalpatel7095@gmail.com>
2020-05-20 14:05:54 -0400

    navigation between pages

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,53 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
-import WeatherApi from '../api/WeatherApi';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import WeatherApi from "../api/WeatherApi";
 
-import SearchBar from '../components/SearchBar';
+import SearchBar from "../components/SearchBar";
 
 const MainScreen = () => {
-    const [term, setTerm] = useState("");
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState("");
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const searchApi = async (searchTerm) => {
+    console.log("hi there");
 
-    const searchApi = async (searchTerm) => {
-        console.log("hi there");
+    try {
+      const response = await WeatherApi.get("?", {
+        params: {
+          q: searchTerm,
+          appid: "d5f2506e7a55b0a90335036fd389c8c4",
+        },
+      });
+      setResults(response.data);
+      console.log(results);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
 
+  useEffect(() => {
+    searchApi("montreal");
+  }, []);
 
-        try {
-            const response = await WeatherApi.get("?", {
-                params: {
-                    q:searchTerm,
-                    appid:'d5f2506e7a55b0a90335036fd389c8c4'
-                },
-            });
-            setResults(response.data);
-             console.log(results);
-        } catch (err) {
-            setErrorMessage("Something went wrong");
-        }
-    };
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      <Text>Main Screen </Text>
+    </View>
+  );
+};
 
-    useEffect(() => {
-        searchApi("");
-    }, []);
+const styles = StyleSheet.create({});
 
-    return(
-        <View>
-            <SearchBar term={term}
-                       onTermChange={setTerm}
-                       onTermSubmit={() => searchApi(term)}
-            />
-            <Text>Main Screen </Text>
-
-        </View>
-    );
-
-}
-
-const styles = StyleSheet.create({
-
-
-});
-
-export  default MainScreen;
+export default MainScreen;
